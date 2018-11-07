b4536c40c4d3b250a81df2d1921bcdcffa990fc3
RudolfGrill <grillrudolf@gmail.com>
2018-11-07 22:27:42 +0100

    basic file stucture added

---
+++ client/client.js
@@ -0,0 +1,25 @@
+console.log("Hello World!");
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  const bark = {
+    name,
+    content,
+  };
+
+  console.log(bark);
+
+  form.style.display = 'none';
+  loadingElement.style.display = '';
+
+
+});
